97e1f4f88db41212eaaa8840a3f9818ecd63179a
tania-developer <69915059+tania-developer@users.noreply.github.com>
2021-02-23 17:47:05 +0600

    react pros

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { getByPlaceholderText } from '@testing-library/react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
+        <Person name='Sakib'></Person>
+        <Person name='Sakib'></Person>
+        <Person name='Sakib'></Person>
+        <Person name='Sakib'></Person>
+        <Person name='Sakib'></Person>
       </header>
     </div>
   );
+}
+
+function Person(pros) {
+  const personStyle={
+    border:'2px solid red',
+    margin:'20px',
+    width: '400px',
+    color: 'yellow'
+  }
+  return(
+    <div style={personStyle}>
+      <h1>Name: {pros.name}</h1>
+      <h3>Hero Of The Year</h3>
+    </div>
+  )
+
+  
 }
 
 export default App;
