3e6442a67643494277ccebd8eef462861eeda52e
tania-developer <69915059+tania-developer@users.noreply.github.com>
2021-02-24 13:20:27 +0600

    dynamically call object

--- src/App.js
+++ src/App.js
@@ -3,34 +3,49 @@ import './App.css';
 import { getByPlaceholderText } from '@testing-library/react';
 
 function App() {
+  const products = [
+    {name:'photoShop', price:'$90'},
+    {name:'Illustrator', price:'$90'},
+    {name:'photoShop', price:'$90'},
+    {name:'photoShop', price:'$90'},
+    {name:'photoShop', price:'$90'}
+  ]
   return (
     <div className="App">
       <header className="App-header">
+        {
+          products.map(pd => <Product prod={pd}></Product>)
+        }
+        {/* <Person name='Sakib'></Person>
         <Person name='Sakib'></Person>
         <Person name='Sakib'></Person>
         <Person name='Sakib'></Person>
-        <Person name='Sakib'></Person>
-        <Person name='Sakib'></Person>
+        <Person name='Sakib'></Person> */}
       </header>
     </div>
   );
 }
 
-function Person(pros) {
-  const personStyle={
-    border:'2px solid red',
-    margin:'20px',
-    width: '400px',
-    color: 'yellow'
+function Product(props) {
+  console.log(props);
+  const productStyle={
+    width:'200px',
+    height:'200px',
+    backgroundColor:'gray',
+    boxShadow:'2px 2px 5px red',
+    color:'black',
+    margin:'20px'
   }
+  const {name,price} = props.prod;
+  console.log(name,price);
   return(
-    <div style={personStyle}>
-      <h1>Name: {pros.name}</h1>
-      <h3>Hero Of The Year</h3>
+    <div style={productStyle}>
+      <h3>name:{name}</h3>
+      <h2>Price:{price}</h2>
     </div>
   )
-
   
 }
+
 
 export default App;
