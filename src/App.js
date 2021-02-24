3fd3bf27e48bba96a1572966ccd47898dff892bd
tania-developer <69915059+tania-developer@users.noreply.github.com>
2021-02-24 16:46:17 +0600

    handler increase and decrease

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import { getByPlaceholderText } from '@testing-library/react';
+import { useState } from 'react';
 
 function App() {
+  const friends = ['kamal','jamal','salma','asad']
   const products = [
     {name:'photoShop', price:'$90'},
     {name:'Illustrator', price:'$90'},
@@ -13,28 +15,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Counter></Counter>
+        <ul>
+          {
+            friends.map(fname => <li>{fname}</li>)
+          }
+        </ul>
         {
           products.map(pd => <Product prod={pd}></Product>)
         }
-        {/* <Person name='Sakib'></Person>
-        <Person name='Sakib'></Person>
-        <Person name='Sakib'></Person>
-        <Person name='Sakib'></Person>
-        <Person name='Sakib'></Person> */}
+        
       </header>
     </div>
   );
 }
 
+function Counter() {
+  const [count, setCount] = useState(0);
+  return(
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={()=> setCount(count-1)}>Decrease</button>
+    </div>
+  )
+}
+
 function Product(props) {
   console.log(props);
   const productStyle={
-    width:'200px',
+    width:'300px',
     height:'200px',
-    backgroundColor:'gray',
-    boxShadow:'2px 2px 5px red',
+    backgroundColor:'lightgray',
+    boxShadow:'2px 2px 5px gray',
     color:'black',
-    margin:'20px'
+    margin:'20px',
   }
   const {name,price} = props.prod;
   console.log(name,price);
